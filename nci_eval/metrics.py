3c63fe9ef687a67f5c800350e3e781b969a992eb
Jan Brabec <janbrabe@cisco.com>
2020-01-06 10:49:29 +0100

    metrics

---
+++ nci_eval/metrics.py
@@ -0,0 +1,40 @@
+import math
+
+
+def precision_from_tpr_fpr(tpr, fpr, positive_prior):
+    """
+    Computes precision for a given positive prevalence from TPR and FPR
+    """
+    return (positive_prior * tpr) / ((positive_prior * tpr) + ((1 - positive_prior) * fpr))
+
+def precision_with_confidence_interval(tpr, fpr, sigma_tpr, sigma_fpr, positive_prior):
+    """
+    Precision with confidence interval for given positive prevalence from provided tpr, fpr, sigma_tpr, sigma_fpr.
+    This is the alpha^2-interval from the paper.
+    """
+    val = precision_from_tpr_fpr(tpr, fpr, positive_prior)
+    low = precision_from_tpr_fpr(max(tpr - sigma_tpr, 0.0000001), min(fpr + sigma_fpr, 0.999999), positive_prior)
+    high = precision_from_tpr_fpr(min(tpr + sigma_tpr, 0.99999999), max(fpr - sigma_fpr, 0.0000001), positive_prior)
+    return val, low, high
+
+def precision_with_confidence_interval_cv(tpr, fpr, cv_tpr, cv_fpr, positive_prior):
+    """
+    Convenience method for precision_with_confidence_interval. Instead of sigmas this takes coefficients of variation.
+    """
+    return precision_with_confidence_interval(tpr, fpr, cv_tpr*tpr, cv_fpr*fpr, positive_prior)
+
+def max_width_of_precision_confidence_interval(tpr, fpr, sigma_tpr, sigma_fpr):
+    """
+    Computes the max-width of precision alpha^2 confidence interval across range of imbalance rates [10^-7, 10^-1].
+    Returns tuple (argmax_positive_prevalence, interval_width)
+    """
+    max_delta = (-1, -1)
+    n = 10000
+    for p in range(n):
+        incr = math.pow(10000000, 1/n)
+        pp = 1 / math.pow(incr, p)
+        _, l, h = precision_with_confidence_interval(tpr, fpr, sigma_tpr, sigma_fpr, pp)
+        delta = (pp, h-l)
+        if delta[1] > max_delta[1]:
+            max_delta = delta
+    return max_delta
---
+++ test/nci_eval/test_metrics.py
@@ -0,0 +1,42 @@
+import pytest
+import nci_eval.metrics as m
+
+
+class TestMetrics:
+    @pytest.mark.parametrize("tpr, fpr, pp, expected", [
+        (0.5, 0.01, 0.01, 0.3355),
+        (0.5, 0.01, 0.001, 0.0477),
+        (0.2, 0.001, 0.03, 0.8608)
+    ])
+    def test_precision_from_tpr_fpr(self, tpr, fpr, pp, expected):
+        assert m.precision_from_tpr_fpr(tpr, fpr, pp) == pytest.approx(expected, 0.001)
+
+    @pytest.mark.parametrize("tpr, fpr, s_tpr, s_fpr, pp, e_v, e_l, e_h", [
+        (0.5, 0.01, 0, 0, 0.01, 0.3355, 0.3355, 0.3355),
+        (0.5, 0.01, 0.1, 0, 0.01, 0.3355, 0.2877, 0.3773),
+        (0.5, 0.01, 0.1, 0.001, 0.01, 0.3355, 0.2686, 0.4024)
+    ])
+    def test_precision_with_confidence_interval(self, tpr, fpr, s_tpr, s_fpr, pp, e_v, e_l, e_h):
+        v, l, h = m.precision_with_confidence_interval(tpr, fpr, s_tpr, s_fpr, pp)
+        v == pytest.approx(e_v, 0.001)
+        l == pytest.approx(e_l, 0.001)
+        h == pytest.approx(e_h, 0.001)
+
+    @pytest.mark.parametrize("tpr, fpr, cv_tpr, cv_fpr, pp, e_v, e_l, e_h", [
+        (0.5, 0.01, 0, 0, 0.01, 0.3355, 0.3355, 0.3355),
+        (0.5, 0.01, 0.2, 0, 0.01, 0.3355, 0.2877, 0.3773),
+        (0.5, 0.01, 0.2, 0.1, 0.01, 0.3355, 0.2686, 0.4024)
+    ])
+    def test_precision_with_confidence_interval_cv(self, tpr, fpr, cv_tpr, cv_fpr, pp, e_v, e_l, e_h):
+        v, l, h = m.precision_with_confidence_interval_cv(tpr, fpr, cv_tpr, cv_fpr, pp)
+        v == pytest.approx(e_v, 0.001)
+        l == pytest.approx(e_l, 0.001)
+        h == pytest.approx(e_h, 0.001)
+
+    @pytest.mark.parametrize("tpr, fpr, s_tpr, s_fpr, e_pp, e_w", [
+        (0.6, 0.001, 0.1*0.6, 0.5*0.001, 0.001047128, 0.31385),
+    ])
+    def test_max_width_of_precision_confidence_interval(self, tpr, fpr, s_tpr, s_fpr, e_pp, e_w):
+        pp, width = m.max_width_of_precision_confidence_interval(tpr, fpr, s_tpr, s_fpr)
+        pp == pytest.approx(e_pp, 0.006)
+        width == pytest.approx(e_w, 0.001)
