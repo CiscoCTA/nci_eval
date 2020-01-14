ae12337373f35e003e8f0ece35caef91e30b73ed
Jan Brabec <janbrabe@cisco.com>
2020-01-14 22:19:49 +0100

    Added conf level monte carlo computation

--- nci_eval/metrics.py
+++ nci_eval/metrics.py
@@ -26,7 +26,8 @@ def precision_with_confidence_interval_cv(tpr, fpr, cv_tpr, cv_fpr, positive_pri
 
 def max_width_of_precision_confidence_interval(tpr, fpr, sigma_tpr, sigma_fpr):
     """
-    Computes the max-width of precision alpha^2 confidence interval across range of imbalance rates [10^-7, 10^-1].
+    Computes the max-width of precision alpha^2 confidence interval (UB(\eta) - LB(\eta)) across range of imbalance
+    rates [10^-7, 10^-1].
     Returns tuple (argmax_positive_prevalence, interval_width)
     """
     max_delta = (-1, -1)
@@ -36,3 +37,29 @@ def max_width_of_precision_confidence_interval(tpr, fpr, sigma_tpr, sigma_fpr):
         if delta[1] > max_delta[1]:
             max_delta = delta
     return max_delta
+
+def conf_level_for_confidence_interval(tpr, fpr, s_tpr, s_fpr, positive_prior, cnt = 2000):
+    """
+    The (UB(\eta) - LB(\eta)) is a alpha^2 confidence interval which means that its confidence level is at least
+    alpha^2. This function estimates the exact confidence level of the interval for given positive_prior. The
+    estimation is performed by repeated sampling of tpr, fpr from normal distributions with standard deviations
+    s_tpr and s_fpr and means at tpr,fpr which should represent the real values. The confidence level is computed
+    by creating confidence intervals around sampled values of precision computed from sampled tpr and fpr. The
+    confidence is the ratio of such confidence intervals that contain the true value of precision computed from
+    real tpr, fpr.
+    """
+    sampled = _sample_precision(tpr, fpr, s_tpr, s_fpr, positive_prior, cnt)
+    v, _, _ = precision_with_confidence_interval(tpr, fpr, s_tpr, s_fpr, positive_prior)
+    return sum(map(lambda x : v >= x[1] and v <= x[2], sampled)) / cnt
+
+def _sample_precision(tpr, fpr, s_tpr, s_fpr, pp, cnt):
+    """
+    Assumes normal distribution in tpr and fpr with standard deviations s_tpr and s_fpr.
+    Samples 'cnt' precisions from it.
+    """
+    res = []
+    for i in range(cnt):
+        tpr_samp = min(1.0, np.random.normal(tpr, s_tpr))
+        fpr_samp = max(0.0000000001, np.random.normal(fpr, s_fpr))
+        res.append(precision_with_confidence_interval(tpr_samp, fpr_samp, s_tpr, s_fpr, pp))
+    return res
--- test/nci_eval/test_metrics.py
+++ test/nci_eval/test_metrics.py
@@ -1,5 +1,6 @@
 import pytest
 import nci_eval.metrics as m
+import numpy as np
 
 
 class TestMetrics:
@@ -18,9 +19,9 @@ class TestMetrics:
     ])
     def test_precision_with_confidence_interval(self, tpr, fpr, s_tpr, s_fpr, pp, e_v, e_l, e_h):
         v, l, h = m.precision_with_confidence_interval(tpr, fpr, s_tpr, s_fpr, pp)
-        v == pytest.approx(e_v, 0.001)
-        l == pytest.approx(e_l, 0.001)
-        h == pytest.approx(e_h, 0.001)
+        assert v == pytest.approx(e_v, 0.001)
+        assert l == pytest.approx(e_l, 0.001)
+        assert h == pytest.approx(e_h, 0.001)
 
     @pytest.mark.parametrize("tpr, fpr, cv_tpr, cv_fpr, pp, e_v, e_l, e_h", [
         (0.5, 0.01, 0, 0, 0.01, 0.3355, 0.3355, 0.3355),
@@ -29,9 +30,9 @@ class TestMetrics:
     ])
     def test_precision_with_confidence_interval_cv(self, tpr, fpr, cv_tpr, cv_fpr, pp, e_v, e_l, e_h):
         v, l, h = m.precision_with_confidence_interval_cv(tpr, fpr, cv_tpr, cv_fpr, pp)
-        v == pytest.approx(e_v, 0.001)
-        l == pytest.approx(e_l, 0.001)
-        h == pytest.approx(e_h, 0.001)
+        assert v == pytest.approx(e_v, 0.001)
+        assert l == pytest.approx(e_l, 0.001)
+        assert h == pytest.approx(e_h, 0.001)
 
     @pytest.mark.parametrize("tpr, fpr, s_tpr, s_fpr, e_pp, e_w", [
         (0.6, 0.001, 0.1*0.6, 0.5*0.001, 0.001047128, 0.31385),
@@ -40,3 +41,12 @@ class TestMetrics:
         pp, width = m.max_width_of_precision_confidence_interval(tpr, fpr, s_tpr, s_fpr)
         pp == pytest.approx(e_pp, 0.006)
         width == pytest.approx(e_w, 0.001)
+
+    @pytest.mark.parametrize("tpr, fpr, s_tpr, s_fpr, pp, min_conf, max_conf", [
+        (0.6, 0.001, 0.1*0.6, 0.5*0.001, 0.001, 0.75, 0.76),
+    ])
+    def test_conf_level_for_confidence_interval(self, tpr, fpr, s_tpr, s_fpr, pp, min_conf, max_conf):
+        np.random.seed(42)
+        conf = m.conf_level_for_confidence_interval(tpr, fpr, s_tpr, s_fpr, pp, 3000)
+        assert conf >= min_conf
+        assert conf <= max_conf
