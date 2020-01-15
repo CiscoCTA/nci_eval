c8e87c1554962482c208a4cf91aa3afa08dc65ae
Jan Brabec <janbrabe@cisco.com>
2020-01-15 11:36:44 +0100

    fix author email to be consistent with PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         '"On Model Evaluation under Non-constant Class Imbalance" by Brabec et al.',
     url="https://github.com/CiscoCTA/nci_eval",
     author='Jan Brabec, Tomas Komarek',
-    author_email='{janbrabe,tomkomar}@cisco.com',
+    author_email='brabecjan91@gmail.com',
     packages=find_packages(exclude=['notebooks', 'test']),
     include_package_data=True,
     install_requires=['matplotlib>=3.1.0', 'numpy>=1.17.3'],
